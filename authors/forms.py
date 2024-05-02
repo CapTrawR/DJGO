a57ad08db524c96eaef9b20285f37d31e85986fb
CapTrawR <goncalorocha1988@gmail.com>
2024-05-02 16:55:46 +0100

    forms para utilizador, urls , e views html e css

--- authors/forms.py
+++ authors/forms.py
@@ -4,4 +4,60 @@ from django.contrib.auth.models import User
 class RegisterForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = '__al__'
+        fields = [
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'password',
+        ]
+
+        #dar outros nomes ao labels
+        labels = {
+            'username': 'Username',
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'email': 'E-mail',
+            'password': 'Password',
+            'username': 'Username',
+        }
+
+        help_texts={
+            'email': 'The e-mail must be valid!!',
+            'username': 'This field is mandatory! 150 characters or less. Letters, numbers and @/./+/-/_ only.!!',
+        }
+
+        error_messages={
+        'username':{
+            'required': 'This field must not be empty!!',
+            },
+        'email':{
+            'required': 'The E-mail must be valid!!',
+            },
+        'password':{
+            'required': 'This field must not be empty!!',
+            }
+        }
+
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Type your First Name here',
+            }),
+
+            'last_name': forms.TextInput(attrs={
+                'placeholder': 'Type your Last Name here'
+            }),
+
+            'username': forms.TextInput(attrs={
+                'placeholder': 'Type your Username here'
+            }),
+
+            'email': forms.TextInput(attrs={
+                'placeholder': 'Type your E-mail here'
+            }),
+
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type your Password here'
+            }),
+
+        }
--- authors/views.py
+++ authors/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from .forms import RegisterForm
+from django.http import Http404
+from django.shortcuts import redirect
 
-# Create your views here.
+# se alguem enviar o metodo post o navegador recebe como post passa para dentro do formolario se nao cria um novo
 def register_view(request):
-   form = RegisterForm()
-   return render(request, 'authors/pages/register_view.html',{
+    register_form_data = request.session.get('register_form_data', None)
+    form = RegisterForm(register_form_data)
+    return render(request, 'authors/pages/register_view.html',{
       'form': form,
    })
+
+# aqui retorna-nos para o form com os respectivos erros 
+def register_create(request):
+    if not request.POST:
+        raise Http404() #cria me um erro 404
+    
+    POST = request.POST
+    request.session['register_form_data'] = POST
+    form = RegisterForm(POST)
+    
+    return redirect('authors:register')
