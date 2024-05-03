0f9d68aac748782ec54286fcb724ca0054bc7f60
CapTrawR <goncalorocha1988@gmail.com>
2024-05-03 15:10:27 +0100

    test fleids

--- authors/test/test_author_register_form.py
+++ authors/test/test_author_register_form.py
@@ -15,8 +15,11 @@ class AuthorRegisterFormUnitTest():
         ('password', 'Type your password'),
         ('repeat_password', 'Repeat your password'),
     ])
+
     # vou buscar os parametros que tenho em cima e testo aqui !!
-    def test_first_name_place_holder_is_correct(self, field, placeholder):
+    def test_fields_place_holder_is_correct(self, field, placeholder):
         form = RegisterForm() # vou buscar o meu register form
         current_placeholder = form[field].field.widget.attrs['placeholder'] # vou ao sitio especifico do meu for e vou buscar o que quero testar
         self.assertEqual(placeholder, placeholder) # testo o value que tenho no placeholder
+
+    
